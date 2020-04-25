35c2cd4b4ae24c5cd4d2bb4b9c617f2b0ba2754a
Roger Clark <roger@rogerclark.net>
2020-04-26 09:48:35 +1000

    Manually merge finnish upate from riku2015

--- firmware/source/user_interface/languages/finnish.h
+++ firmware/source/user_interface/languages/finnish.h
@@ -20,7 +20,7 @@
  * Translators: OH1E
  *
  *
- * Rev: 12 
+ * Rev: 13 
  */
 #ifndef USER_INTERFACE_LANGUAGES_FINNISH_H_
 #define USER_INTERFACE_LANGUAGES_FINNISH_H_
@@ -111,7 +111,7 @@ const stringsTable_t finnishLanguage =
 .band_limits		= "Bändi Rajoitu", 	// MaxLen: 16 (with ':' + .on or .off)
 .beep_volume		= "NäpÄäniVoim", 	// MaxLen: 16 (with ':' + -24..6 + 'dB')
 .dmr_mic_gain		= "DMR MicGain", 	// MaxLen: 16 (with ':' + -33..12 + 'dB')
-.fm_mic_gain		= "FM mic", 		// MaxLen: 16 (with ':' + 0..31)
+.fm_mic_gain		= "FM MicGain", 	// MaxLen: 16 (with ':' + 0..31)
 .key_long		= "Näp pitkä",	 	// MaxLen: 11 (with ':' + x.xs fitted)
 .key_repeat		= "Näp toisto", 	// MaxLen: 11 (with ':' + x.xs fitted)
 .dmr_filter_timeout	= "Suodin aika", 	// MaxLen: 16 (with ':' + 1..90 + 's')
@@ -152,8 +152,8 @@ const stringsTable_t finnishLanguage =
 .dmr_beep		= "DMR piippi", 	// MaxLen 16 (with ':' + .star/.stop/.both/.none)
 .start			= "Alku", 		// MaxLen 16 (with ':' + .dmr_beep)
 .both			= "Molemm",		// MaxLen 16 (with ':' + .dmr_beep)
-.vox_threshold          = "VOX Thres.",         // MaxLen 16 (with ':' + .off or 1..30)
-.vox_tail               = "VOX Tail"            // MaxLen 16 (with ':' + .n_a or '0.0s')
+.vox_threshold          = "VOX Herkk.",		// MaxLen 16 (with ':' + .off or 1..30)
+.vox_tail               = "VOX Viive"		// MaxLen 16 (with ':' + .n_a or '0.0s')
 };
 /********************************************************************
  *
